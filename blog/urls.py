e5b953c662162c3385c04f1d4c7e7f39e403e7d3
hem <flyinghem@gmail.com>
2020-05-06 07:51:53 +0000

    changes comit

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,6 @@ from . import views
 urlpatterns = [
 # assigning post_list view to the root URL
 path('', views.post_list, name = 'post_list'), 
+path('post/<int:pk>', views.post_detail, name ='post_detail'),
 
 ]
