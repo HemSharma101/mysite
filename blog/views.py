17aad8c5ca17d6c03f11ac44f9351089b9fa9851
Hem Sharma <flyinghem@gmail.com>
2020-05-06 17:25:13 +1000

    added CSS/bootstrap,template extention, view and template to display detil post

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -15,3 +15,12 @@ def post_list(request):
 	#which is a list of post model object
 
 	return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+# view has given extra parameter, pk.
+# The function has to catch it along with request 
+# pk must have same name as defined in the urls 
+def post_detail(request, pk):
+	'''this function displays detailed post when clicked on the post list'''
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post })
